07c85361c83715d4c5663ac82ab8505f4cc27ad6
Vitalik Shirokov <vitalik.shirokov@gmail.com>
2022-08-17 19:16:55 +0300

    Bitrix v21.400 (#10)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,10 +4,14 @@ use Sheerockoff\BitrixCi;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-if (!getenv('SKIP_MIGRATION')) {
-    echo "Migration...";
+if (!getenv('SKIP_MIGRATION', true) && !getenv('SKIP_MIGRATION')) {
+    file_put_contents('php://stdout', 'Migration...');
     BitrixCi\Bootstrap::migrate();
-    echo "\e[92mCOMPLETE\e[0m\n";
+    file_put_contents('php://stdout', "\e[92mCOMPLETE\e[0m\n");
 }
 
 BitrixCi\Bootstrap::bootstrap();
+
+while (ob_get_level()) {
+    ob_end_clean();
+}
